bf88c70248d648defb80bd85c2055dacafad0a90
Mike Morton <mmorton7295@gmail.com>
2021-07-03 14:49:41 -0500

    creates simple conditional function

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+let y = 2
+let x = 10
+let z = 12
+
+function compareToTen(n) {
+  if (n == 10) {
+    console.log("equal to 10")
+  } else if (n < 10) {
+    console.log("less than 10")
+  } else if (n > 10) {
+    console.log("greater than 10")
+  }
+}
+compareToTen(x)
+compareToTen(y)
+compareToTen(z)
